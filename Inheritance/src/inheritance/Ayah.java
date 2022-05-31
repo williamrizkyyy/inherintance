ea0d4ea1208ccf47d3a58a0a14be5545f8598745
williamrizkyyy <101534537+williamrizkyyy@users.noreply.github.com>
2022-05-31 09:41:17 +0700

    Add files via upload

---
+++ Inheritance/src/inheritance/Ayah.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+/**
+ *
+ * @author CATURWARGA COMPUTER
+ */
+public class Ayah {
+        String Sifat= "Penyabar";
+    double TB= 173.1;
+    
+    void hobbi(){
+        System.out.println("Hobi nya adalah Ngo-piding=Ngopi dan Ngoding");
+    }
+
+}
---
+++ Inheritance/src/inheritance/Hasil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+/**
+ *
+ * @author CATURWARGA COMPUTER
+ */
+public class Hasil {
+    public static void main(String[] args) {
+        Anak dina=new Anak();
+        dina.setnama("Dinaa");
+        dina.Cetak();
+    }
+
+
+
+    
+}
